ff4ca951d841dbaf545e70322e17785ca80c8e93
bill gates <hannesp@ppedv.de>
2018-06-06 11:53:44 +0200

    Mail Senden

---
+++ WebformsBGH/modul04/mailsenden.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebformsBGH.modul04
+{
+    public partial class mailsenden : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var mail = new SmtpClient();
+            mail.Send("asp@ppedv.de", "hannesp@ppedv.de", "Betreff", "Text");
+
+        }
+    }
+}
