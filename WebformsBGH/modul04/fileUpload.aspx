222b23f134f5ea838181370e1334dd808f0781a9
bill gates <hannesp@ppedv.de>
2018-06-06 11:37:49 +0200

    Fileupload mit GUID

---
+++ WebformsBGH/modul04/fileUpload.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="fileUpload.aspx.cs" Inherits="WebformsBGH.modul04.fileUpload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>datei hochladen
+            <asp:FileUpload ID="FileUpload1" runat="server"   />
+            <asp:Button ID="Button1" runat="server" Text="upload" 
+                
+                OnClick="Button1_Click" />
+            
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebformsBGH/modul04/fileUpload.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebformsBGH.modul04
+{
+    public partial class fileUpload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                var id = Guid.NewGuid().ToString();
+
+                FileUpload1.PostedFile.SaveAs(
+                    Server.MapPath("~/app_data/upload/" +
+                     id+ ".png"));
+                Label1.Text = "die Datei " + FileUpload1.PostedFile.FileName
+                    + "wurde unter" + id + " gespeichert";
+            }
+        }
+    }
+}
