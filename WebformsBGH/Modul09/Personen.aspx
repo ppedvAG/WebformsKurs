00d367e1d8e4963984fa9ef5a52dd99ee1c34cb0
bill gates <hannesp@ppedv.de>
2018-06-07 14:43:49 +0200

    Detaisl View Model Binding

---
+++ WebformsBGH/Modul09/Personen.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Modul07/Site1.Master" AutoEventWireup="true" CodeBehind="Personen.aspx.cs" Inherits="WebformsBGH.Modul09.Personen1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:FormView ID="FormView1" runat="server"
+        ItemType="WebformsBGH.Modul09.Personen"
+        SelectMethod="FormView1_GetItem"
+        UpdateMethod="FormView1_UpdateItem"
+        DataKeyNames="Id">
+        <ItemTemplate>
+            <%#Item.Id %><br />
+            <%#Item.Bearbeiter%><br />
+            <%#Item.FullName %><br />
+            <%#Item.Ort %><br />
+            <asp:Button ID="Button1" runat="server" CommandName="Edit" Text="Edit" />
+        </ItemTemplate>
+        <EditItemTemplate>
+            <%#Item.Id %><br />
+            <%#Item.Bearbeiter%><br />
+            <asp:TextBox ID="TextBox1" runat="server" Text="<%#BindItem.FullName %>"></asp:TextBox><br />
+            <asp:TextBox ID="TextBox2" runat="server" Text="<%#BindItem.Ort %>"></asp:TextBox><br />
+             <asp:Button ID="Button3" runat="server" Text="cancel"
+                CommandName="cancel"                />
+            <asp:Button ID="Button2" runat="server" Text="speichern"
+                CommandName="update"/>
+        </EditItemTemplate>
+    </asp:FormView>
+
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="Scripts" runat="server">
+</asp:Content>
---
+++ WebformsBGH/Modul09/Personen.aspx.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNet.FriendlyUrls.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebformsBGH.Modul09
+{
+    public partial class Personen1 : System.Web.UI.Page
+    {
+        DBModel ef = new DBModel();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Der ID-Parameter sollte dem DataKeyNames-Wert entsprechen, der für das Steuerelement
+        // festgelegt wurde, oder mit einem Wertanbieterattribut versehen werden, z. B. [QueryString]int id
+        public WebformsBGH.Modul09.Personen FormView1_GetItem([FriendlyUrlSegments(0)] int id=1)
+        {
+
+            return ef.Personen.Find(id);
+        }
+
+        // Der Name des ID-Parameters sollte dem für das Steuerelement festgelegten DataKeyNames-Wert entsprechen.
+        public void FormView1_UpdateItem(int id)
+        {
+            WebformsBGH.Modul09.Personen item = null;
+            item = ef.Personen.Find(id);
+            if (item == null)
+            {
+                // Das Element wurde nicht gefunden.
+                ModelState.AddModelError("", String.Format("Das Element mit der ID {0} wurde nicht gefunden.", id));
+                return;
+            }
+            TryUpdateModel(item);
+            if (ModelState.IsValid)
+            {
+                if (item.Ort=="")
+                {
+                    item.Ort = "Burghausen";
+                }
+                 ef.SaveChanges();
+
+            }
+        }
+    }
+}
