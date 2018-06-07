1e609eddb50c4b5feb5e493afd132490bf497b81
bill gates <hannesp@ppedv.de>
2018-06-07 11:58:23 +0200

    Model Binding

---
+++ WebformsBGH/Modul09/ToDoModelBinding.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ToDoModelBinding.aspx.cs" Inherits="WebformsBGH.Modul09.ToDoModelBinding" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+              <ul>
+            <asp:Repeater ID="rptToDo"
+                ItemType="mbToDo"
+                SelectMethod="rptToDo_GetData"
+               
+                runat="server">
+                <ItemTemplate>
+                    <li></li>
+                </ItemTemplate>
+            </asp:Repeater>
+
+        </ul>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebformsBGH/Modul09/ToDoModelBinding.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebformsBGH.Modul09
+{
+    public partial class ToDoModelBinding : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public IEnumerable<mbToDo> rptToDo_GetData()
+        {
+            var liste = new List<mbToDo>();
+            using (SqlConnection con = new SqlConnection(
+              ConfigurationManager.ConnectionStrings["WebformsDBConnectionString1"].ConnectionString))
+            {
+                var cmd = new SqlCommand("select * from Todo ", con);
+                con.Open();
+                var rd = cmd.ExecuteReader();
+
+                while (rd.Read())
+                {
+                    liste.Add(new mbToDo()
+                    {
+                        Id = (int)rd["Id"],
+                        Aufgabe = rd["Aufgabe"].ToString(),
+                        Erledigt = (bool)rd["Erledigt"]
+                    }
+                        );
+                }
+            }
+            return liste;
+        }
+    }
+    public class mbToDo
+    {
+        public int Id { get; set; }
+        public string Aufgabe { get; set; }
+        public bool? Erledigt { get; set; }
+
+    }
+}
