0e95e220554c3919441b5df453cb8b98354d6249
bill gates <hannesp@ppedv.de>
2018-06-07 11:40:58 +0200

    generische Liste mit SQLCommand und Datareader

---
+++ WebformsBGH/Modul09/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Modul07/Site1.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebformsBGH.Modul09.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <div class="row">
+        <div class="col">
+            <h1>Todo Items</h1>
+            <div class="list-group">
+                <asp:Repeater ID="rptToDo" runat="server">
+                    <ItemTemplate>
+                        <a class="list-group-item list-group-item-action"
+                            href='todoitem/<%#Eval("Id") %>'>
+                           
+                    <%#Eval("Aufgabe") %> </a>
+                    </ItemTemplate>
+                </asp:Repeater>
+
+            </div>
+        </div>
+    </div>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="Scripts" runat="server">
+</asp:Content>
---
+++ WebformsBGH/Modul09/WebForm1.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebformsBGH.Modul09
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var liste = new List<MyToDo>();
+            using (SqlConnection con = new SqlConnection(
+              ConfigurationManager.ConnectionStrings["WebformsDBConnectionString1"].ConnectionString))
+            {
+                var cmd = new SqlCommand("select * from Todo ", con);
+                con.Open();
+                var rd = cmd.ExecuteReader();
+         
+                while (rd.Read())
+                {
+                    liste.Add(new MyToDo()
+                    {
+                        Id = (int)rd["Id"],
+                        Aufgabe=rd["Aufgabe"].ToString(),
+                        Erledigt=(bool) rd["Erledigt"]
+
+                    }
+                        );
+                }
+            }
+            rptToDo.DataSource = liste;
+            rptToDo.DataBind();
+
+        }
+    }
+    public class MyToDo
+    {
+        public int Id { get; set; }
+        public string Aufgabe { get; set; }
+        public bool? Erledigt { get; set; }
+
+    }
+}
