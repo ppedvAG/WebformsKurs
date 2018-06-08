ed1b6b4f8374b7cab45f71d84c93af9f0a821eb0
bill gates <hannesp@ppedv.de>
2018-06-08 13:42:47 +0200

    Page Render Caching

---
+++ WebformsBGH/Modul14/WebForm2.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Modul07/Site1.Master" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="WebformsBGH.Modul14.WebForm2" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <table>
+        <asp:Repeater ID="Repeater1" runat="server"
+            SelectMethod="Repeater1_GetData"
+            ItemType="WebformsBGH.Modul05.PlzOrt">
+            <ItemTemplate>
+                <tr>
+                    <td><%#Item.Plz %></td>
+                    <td><%#Item.Ort %></td>
+                </tr>
+            </ItemTemplate>
+        </asp:Repeater>
+    </table>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="Scripts" runat="server">
+</asp:Content>
---
+++ WebformsBGH/Modul14/WebForm2.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebformsBGH.Modul05;
+
+namespace WebformsBGH.Modul14
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public IEnumerable<WebformsBGH.Modul05.PlzOrt> Repeater1_GetData()
+        {
+            var Liste = new List<PlzOrt>();
+            var datei = Server.MapPath("~/app_data/plzort.csv");
+            var arr = File.ReadAllLines(datei);
+            foreach (var item in arr)
+            {
+                var f = item.Split(';');
+                Liste.Add(new PlzOrt()
+                {
+                    Plz = Convert.ToInt32(f[0]),
+                    Ort = f[1]
+                });
+            }
+            var p = Request.QueryString["suche"];
+            if (p!=null)
+            {
+                var q = from o in Liste
+                        where o.Ort.Contains(p)
+                        select o;
+                return q.ToList();
+            }
+            return Liste;
+        }
+    }
+}
