4bfb7f8e9a69e371220607333ae28805c73974c5
bill gates <hannesp@ppedv.de>
2018-06-08 09:45:47 +0200

    Autocomplete

---
+++ WebformsBGH/Modul15/AutoCompleteOrt.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Modul07/Site1.Master" AutoEventWireup="true" CodeBehind="AutoCompleteOrt.aspx.cs" Inherits="WebformsBGH.Modul15.AutoCompleteOrt" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="../Content/jquery-ui.css" rel="stylesheet" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="Scripts" runat="server">
+    <script src="../Scripts/jquery-ui.js"></script>
+</asp:Content>
---
+++ WebformsBGH/Modul15/AutoCompleteOrt.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using WebformsBGH.Modul05;
+
+namespace WebformsBGH.Modul15
+{
+    public partial class AutoCompleteOrt : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+        [WebMethod]
+        public static List<PlzOrt> CompleteText(string Par)
+        {
+            
+            var Liste = new List<PlzOrt>();
+            var arr = File.ReadAllLines(HttpContext.Current.Server.MapPath(
+                "~/app_data/plzort.csv"));
+            foreach (var item in arr)
+            {
+                var f = item.Split(';');
+                Liste.Add(new PlzOrt()
+                {
+                    Plz = Convert.ToInt32(f[0]),
+                    Ort = f[1]
+                });
+            }
+            var q = from o in Liste
+                    where o.Ort.Contains(Par)
+                    select x;
+            return q;
+
+        }
+    }
+}
